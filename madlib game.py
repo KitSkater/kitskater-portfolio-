b6e8a87061c5e77c3e63a08b725014b6016a4cb7
KitSkater <william.robert.strittmatter@gmail.com>
2025-01-23 20:37:42 -0600

    madlib game with python

---
+++ madlib game.py
@@ -0,0 +1,12 @@
+# madlibe game 
+#word game where you create a story
+# by filling in the blanks 
+adjective1 = input("Enter an adjective: ") 
+noun1 = input("Enter a noun: ")
+adjective2 = input("Enter an adjective: ") 
+verb1 = input("Enter a verb: ") 
+adjective3 = input("Enter an adjective: ") 
+print(f"Today I went to a {adjective1} zoo.") 
+print(f"in a exhibit i saw a {noun1}") 
+print(f"{noun1} was {adjective2} and {verb1}") 
+print(f"i was {adjective3}") 
